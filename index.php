bf99b73d0d85e487474a2e41db00e7921b6d15a1
songohansmaka@gmail.com <songohansmaka@gmail.com>
2022-10-11 14:30:01 +0200

    Cannot access subdirectories yet.

--- index.php
+++ index.php
@@ -12,28 +12,76 @@
 <body>
   <?php
   $root = "./";
-
-    if (isset($_GET["dest"])) {
-      $dir = $_GET["dest"];
-    } else {
-      $dir = $root;
+  $path_arr = array("$root");
+  $n = 0;
+  if (isset($_GET["dest"])) {
+    $dest = $_GET["dest"];
+    array_push($path_arr, $dest);
+    $path = join("", $path_arr);
+    $path_arr_new = $path_arr;
+    $n += 1;
+    $_GET["dest"] = NULL;
+    if (isset($_GET["dest"]) and $n > 0) {
+      $dest = $_GET["dest"];
+      array_push($path_arr_new, $dest);
+      $path = join("", $path_arr_new);
+      $_GET["dest"] = NULL;
     }
+  } else {
+    $path = $root;
+  }
+  echo $path;
 
-  $dir_scan = scandir($dir);
+/*   if (isset($_GET["dest"]) and !isset($path_arr_new)) {
+    $dir = $_GET["dest"];
+    array_push($path_arr, $dir);
+    #echo"<p>in</p>";
+    #echo $_GET["dest"]."<br>";
+    #$_GET["dest"] = NULL;
+    #var_dump($_GET["dest"]);
+    $dir = join("/", $path_arr);
+    $path_arr_new = $path_arr;
+    #echo "kakaa";
+    if (isset($_GET["dest"]) and isset($path_arr_new)) {
+      #echo "isset";
+      print_r($path_arr_new);
+      array_push($path_arr_new, $dir);
+      $dir = join("/", $path_arr);
+      #$dir = $path_arr_new;
+      $_GET["dest"] = NULL;
+      }
+  } else {
+    $dir = $root;
+  } */
+
+  echo "<p>";
+  print_r($path_arr);
+  echo "</p>";
+  #echo "<p>out</p>";
+  #var_dump($_GET["dest"]);
+
+  $dir_scan = scandir($path);
 
   foreach ($dir_scan as $item) {
-    if (is_dir("$dir/$item")) {
+    #$item = $item."/";
+    if (is_dir("$path/$item")) {
       if ($item == ".") {
         #Pass
       } elseif ($item == "..") {
         echo "<h2>&larr; Parent folder</h2>";  
+      } elseif (str_starts_with("$item",".")) {
+        echo "<p><del>Folder \"$item\" - Hidden directory</del></p>";
       } else {
-        echo "<h3><a href=\"?dest=$item\">$item</a></h3>";
+        echo "<h3><a href=\"?dest=$item/\">$item</a></h3>";
       }
+    } else {
+    if (str_starts_with("$item",".")) {
+      echo "<p><del>File \"$item\" - Hidden file</del></p>";
     } else {
       echo "<pre>[Item \"$item\" - not a directory]</pre>";
     }
   }
+}
 
 
   ?>
